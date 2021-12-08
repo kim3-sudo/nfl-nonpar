b4af1b41b0b582e185e225c64619661cf6834a6c
Sejin Kim <kim3@kenyon.edu>
2021-12-08 10:40:38 -0500

    Add mice procedure

--- src/analysis.R
+++ src/analysis.R
@@ -3,13 +3,12 @@
 ### STAT 216 F21 @ Kenyon College
 
 # Load libraries
-library(mosaic)
-library(NSM3)
-library(mice)
-library(Kendall)
-library(trend)
-library(dplyr)
-library(matrixStats)
+library(mosaic) # For EDA
+library(NSM3) # For Durbin-Skillings-Mack
+library(mice) # For MICE procedure
+library(Kendall) # For Mann-Kendall
+library(trend) # For Sen's slope
+library(PMCMRplus) # For Skillings-Mack rank sum test for partially balacned incomplete block designs
 
 # Load data
 data <- readRDS(url('https://github.com/kim3-sudo/nfl_analysis_data/blob/main/nflfastr_pbp_2010_to_2020.rds?raw=true'))
@@ -23,9 +22,10 @@ nfl <- nfl[, c("game_id"
                , "passing_yards"
                , "return_yards"
                , "air_yards")]
-nfl$yards_gained <- rowMaxs(as.matrix((nfl[, c(4, 5, 6, 7)])))
+nfl$max_yards <- pmax(nfl$kick_distance, nfl$passing_yards, nfl$return_yards, nfl$air_yards, na.rm = TRUE)
+mousey <- mice(nfl)
+nfl <- complete(mousey)
 View(nfl)
-
 
 # EDA
 
@@ -34,11 +34,12 @@ View(nfl)
 ## Response: Distance (*_yards)
 ## Blocks: Game (game_id)
 ## Treatments: Stadiums (stadium_id)
+pMackSkil(nfl$max_yards, nfl$stadium_id, nfl$game_id)
 
 # Durbin-Skillings-Mack Procedure
 ## Distribution-Free Test for General Alternatives in a Randomized Balanced Incomplete Block Design
 ## Response: Distance (*_yards)
 ## Blocks: Game (game_id)
 ## Treatments Stadiums (stadium_id)
-
+pDurSkiMa(nfl$max_yards, b = nfl$game_id, trt = nfl$stadium_id, method = "Monte Carlo", n.mc = 1000)
 # 
